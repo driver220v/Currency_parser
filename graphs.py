32371237895fcdc6d450db831e50c13680a622f3
driver220v <driver220v@gmail.com>
2019-10-29 20:34:37 +0300

    initial commit

---
+++ graphs.py
@@ -0,0 +1,116 @@
+from psycopg2 import connect
+import matplotlib.pyplot as plt
+
+
+def connection_db():
+    con = connect(
+        database="Currencies",
+        user="postgres",
+        password='911',
+        host="127.0.0.1",
+        port="5432"
+    )
+    return con
+
+
+def choose_unit(p_table):
+    table = ''
+    units = {1: 'USD',
+         2: 'EUR',
+         3: 'DKK',
+         4: 'CAD',
+         5: 'SEK',
+         6: 'CHF',
+         7: 'JPY',
+         8: 'AUD'}
+    for key in units:
+        if key == p_table:
+            table = units[key]
+    return table
+
+
+def take_data(
+    con, table,
+    time_begin, time_end
+):
+    
+    cur = con.cursor()
+    cur.execute(f'''Select * from {table}
+                    where renew_date between '{time_begin}' and '{time_end}' ''')
+    rows = cur.fetchall()
+    return rows
+
+
+def graph(rows, table):
+    purchase_course = []
+    sale_course = []
+    dates = []
+    for i in rows:
+        purchase_course.append(i[0])
+        sale_course.append(i[1])
+        dates.append(f'{i[2].day}.{i[2].month}.{i[2].year} '
+                     f'{i[2].hour}:{i[2].minute}')
+
+    plt.style.use('fivethirtyeight')
+    plt.figure(figsize=(12, 7))
+    sale_course = plt.plot(dates,
+                           sale_course,
+                           color='r',
+                           linewidth=3,
+                           marker='o',
+                           label='Sale course')
+
+    purchase_course = plt.plot(dates,
+                               purchase_course,
+                               color='g',
+                               linewidth=3,
+                               marker='o',
+                               label='Purchase course')
+    plt.legend()
+
+    plt.xlabel('date')
+    plt.ylabel('price')
+    plt.title(f'{table} exchange rate')
+
+    purchase_course.clear()
+    sale_course.clear()
+    dates.clear()
+    plt.show()
+
+
+con = connection_db()
+
+print('1 - USD',
+      '2 - EUR',
+      '3 - DKK',
+      '4 - CAD',
+      '5 - SEK',
+      '6 - CHF',
+      '7 - JPY',
+      '8 - AUD',
+      sep='\n'
+      )
+p_table = int(input(
+    'For exit press 0. '
+    'Input number of unit: '
+))
+# Enter dates between which exchange rate will be displayed
+time_begin, time_end = (
+        input('Input intital date in "dd.mm.yyyy" format: '),
+        input('Input terminal date in "dd.mm.yyyy" format: ')
+        )
+
+while 1 <= p_table < 9:
+    table = choose_unit(p_table)
+    rows = take_data(con, table,
+                     time_begin, time_end)
+    graph(rows, table)
+    p_table = int(input('For exit press 0. Enter number of unit: '))
+    if p_table != 0:
+        time_begin, time_end = (
+        input('Input initial date in "dd.mm.yyyy" format: '),
+        input('Input terminal date in "dd.mm.yyyy" format: '))
+    else:
+        pass
+else:
+    pass
---
+++ parser.py
@@ -0,0 +1,96 @@
+import requests
+from psycopg2 import connect
+from psycopg2.extensions import AsIs
+
+
+def add_items(url):
+    r = requests.get(url)
+    beacon = r.text.find('<section class="widget" data-test="exchange-office-rates">')
+    data = {}
+
+    currencies = ['USD',
+                  'EUR',
+                  'AUD',
+                  'CAD',
+                  'DKK',
+                  'SEK',
+                  'CHF',
+                  'JPY']
+
+    for cur in currencies:
+        unit_search = r.text.find(cur, beacon)
+        if unit_search == -1:
+            continue
+        td_tag_open = '<td class="font-size-large">'
+        td_tag_close = '</td>'
+
+        purchase_cr_open_tag = r.text.find(td_tag_open,
+                                           unit_search)
+        purchase_cr_close_tag = r.text.find(td_tag_close,
+                                            purchase_cr_open_tag)
+
+        sale_cr_open_tg = r.text.find(td_tag_open,
+                                      purchase_cr_close_tag)
+        sale_cr_close_tg = r.text.find(td_tag_close,
+                                       sale_cr_open_tg)
+
+        td_tag_open_time = '<td class="color-border-dark font-size-default">'
+        time_open = r.text.find(td_tag_open_time,
+                                sale_cr_close_tg)
+        time_close = r.text.find(td_tag_close,
+                                 time_open)
+
+        assert (purchase_cr_open_tag < sale_cr_open_tg
+                and purchase_cr_close_tag < sale_cr_open_tg
+                and time_open < time_close)
+        i2 = purchase_cr_open_tag + len(td_tag_open)
+        i3 = sale_cr_open_tg + len(td_tag_open)
+        i4 = time_open + len(td_tag_open_time)
+
+        data[cur] = (float(r.text[i2:purchase_cr_close_tag].strip().replace(',', '.')),
+                  float(r.text[i3:sale_cr_close_tg].strip().replace(',', '.')),
+                  str(r.text[i4:time_close].strip()))
+    return data
+
+
+def connection_db():
+    con = connect(
+        database="Currencies",
+        user="postgres",
+        password='911',
+        host="127.0.0.1",
+        port="5432")
+    return con
+
+
+def create_tables(con, data):
+    query = """Create table %(table)s
+      (purchase_course   real,
+       sale_course       real,
+       renew_date        timestamp )"""
+    for key in data:
+        cur = con.cursor()
+        try:
+            cur.execute(query, {'table': AsIs(key)})
+        except:  # Prevent program from creating tables twicely
+            con.commit()
+            cur.close()
+
+
+def insert_into_tables(con, data):
+    for key, value in data.items():
+        cur = con.cursor()
+        query = f"""Insert into {key}(purchase_course, sale_course, renew_date)
+                values({value[0]}, {value[1]}, '{value[2]}')"""
+        cur.execute(query)
+        con.commit()
+        cur.close()
+
+
+url = 'https://www.banki.ru/products/currency/bank/mkb/moskva/'
+data = add_items(url)
+con = connection_db()
+cur = create_tables(con, data)
+insert_Tables = insert_into_tables(con, data)
+
+con.close()
